c91078544d2370b9f8e63f52f5b4218da1369401
Joseph Schmidt <schmi150@purdue.edu>
2024-11-04 23:03:07 -0500

    working on cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { useSelector, useDispatch, useStore } from "react-redux";
+import { Button, Form } from "react-bootstrap";
+import { useSelector, useStore } from "react-redux";
 
 function Cart() {
   let selector = useSelector((state) => state.cart);
   const store = useStore();
-  console.log(store.getState().cart);
+  selector = store.getState().cart;
+
+  const card_number = "1234567890123456";
+  console.log(
+    card_number
+      .replace(/\D/g, "")
+      .replace(/(.{4})/g, "$1-")
+      .trim()
+      .slice(0, -1)
+  );
+
+  function handleCheckout(e) {
+    e.preventDefault();
+    console.log(e.target.elements.card_num.value);
+  }
+
   return (
     <div>
       <h1>Sphagetti Shack Catering!!!</h1>
+      {selector.map((item, i) => {
+        return <h2 key={i}>{item.menu_item + " " + item.menu_price}</h2>;
+      })}
+
+      <Form onSubmit={handleCheckout}>
+        <Form.Group controlId="card_num">
+          <Form.Label>Card Number</Form.Label>
+          <Form.Control type="text" placeholder="xxxx-xxxx-xxxx-xxxx" />
+        </Form.Group>
+        <Form.Group controlId="card_cvv">
+          <Form.Label>CVV</Form.Label>
+          <Form.Control type="text" placeholder="123" />
+        </Form.Group>
+        <Form.Group controlId="expiry_date">
+          <Form.Label>Expiry Date</Form.Label>
+          <Form.Control type="text" placeholder="MM/YYYY" />
+        </Form.Group>
+        <Button type="submi">Place Order</Button>
+      </Form>
     </div>
   );
 }
